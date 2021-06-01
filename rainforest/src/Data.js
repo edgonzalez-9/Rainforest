510bd3a0669d399805ac500412b07d5cc172d7b5
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-06-01 18:51:57 -0500

    created three dummy entries to set up product pages.

---
+++ rainforest/src/Data.js
@@ -0,0 +1,29 @@
+let Products = [
+    {
+        name: 'Earth',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/1200px-The_Earth_seen_from_Apollo_17.jpg',
+        seller: 'Mother Nature',
+        description: 'Used but still salvageable, unruly tenants.',
+        reviews: [
+            '5/7 - Would visit again.', 'Gets hot during summer, recommend inventing air conditioning.', 'Oil is bad.'
+        ]
+    },
+    {
+        name: 'Moon',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg',
+        seller: 'Artemis',
+        description: 'Shy but has great pull amongst the ocean loving folks.',
+        reviews: [
+            'Keeps you company on dark nights(some of the time)', 'Avoid exposure if you have been exposed to lycans.'
+        ]
+    },
+    {
+        name: 'Sun',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/The_Sun_by_the_Atmospheric_Imaging_Assembly_of_NASA%27s_Solar_Dynamics_Observatory_-_20100819.jpg/440px-The_Sun_by_the_Atmospheric_Imaging_Assembly_of_NASA%27s_Solar_Dynamics_Observatory_-_20100819.jpg',
+        seller: 'Apollo',
+        description: `Don't stare at, touch, or taste.`,
+        reviews: [
+            'OH GOD I SHOULD HAVE READ THE PRODUCT DESCRIPTION MY EYES AHHHHHHHHHHHHH.'
+        ]
+    }
+]
