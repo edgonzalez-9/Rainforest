e7be4cf55937956f8bd1ed34149c45cfcc7957a3
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-06-02 20:05:22 -0500

    added a click function to the cards

--- rainforest/src/Components/ProductCard.js
+++ rainforest/src/Components/ProductCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ProductCard(props){
     return (
-        <div className="card" style={{width: `20vw`, margin: `10px`}}>
+        <div className="card" style={{width: `20vw`, margin: `10px`}} onClick={() => console.log("Hello")}>
             <img src={props.product.image} className="card-img-top" alt=""/>
             <div className="card-body">
                 <p className="card-title">{props.product.name}</p>
