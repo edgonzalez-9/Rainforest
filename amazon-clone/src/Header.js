a31d4457c0739cb891d96c7becb02ff28206badc
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-06-10 03:19:38 -0500

    created header component

---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Header.css"
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <nav className="header">
+            <Link to="/">
+                <img 
+                    className="header__logo" 
+                    src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+                    alt=""
+                />
+            </Link>
+            <input 
+                type="text" 
+                className="header__searchInput"
+            />
+        </nav>
+    )
+}
+
+export default Header
