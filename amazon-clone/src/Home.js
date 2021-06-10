e9a22fd83ff67a2c4a709353707e83478637304c
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-06-10 06:26:05 -0500

    working on product component

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Product from "./Product.js"
 import "./Home.css";
 
 function Home() {
@@ -9,6 +10,56 @@ function Home() {
                 src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/NTYxZWIwNDMt/NTYxZWIwNDMt-ZjA5YjMyNmIt-w3000._CB668766246_.jpg" 
                 alt=""
             />
+            <div className="home__row">
+                <Product
+                    id="123456789"
+                    title="Way of Kings"
+                    price={19.99}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/91KzZWpgmyL.jpg"
+                />
+                <Product
+                    id="123456789"
+                    title="Mario Kart 8 Deluxe"
+                    price={49.99}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/71zMv5%2Brx5S._SL1500_.jpg"
+                />
+            </div>
+
+            <div className="home__row">
+                <Product
+                    id="123456789"
+                    title="KitchenAid KSMMGA Metal Food Grinder Attachment, 2.5 lb, Silver"
+                    price={79.99}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/81cPC8tWkpL._AC_SL1500_.jpg"
+                />
+                <Product
+                    id="123456789"
+                    title="Ring Video Doorbell Wired"
+                    price={59.99}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/518CV1NgeZL._SL1000_.jpg"
+                />
+                <Product
+                    id="123456789"
+                    title="Sauder North Avenue Cart, Smoked Oak finish"
+                    price={89.99}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/714dbcEit9L._AC_SL1500_.jpg"
+                />
+            </div>
+
+            <div className="home__row">
+                <Product
+                    id="123456789"
+                    title="Greenies Feline Natural Dental Care Cat Treats, Chicken Flavor, All Bag Sizes"
+                    price={19.99}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/8126aXqeFyL._AC_SL1500_.jpg"
+                />
+            </div>
         </div>
     )
 }
