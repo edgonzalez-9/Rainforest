6c2bb5bfcdae50488231c1a583eaceea7bc44e5e
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-06-10 05:30:55 -0500

    uissues with modules

---
+++ amazon-clone/src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="home">
+            <img 
+                className="home_image"
+                src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/NTYxZWIwNDMt/NTYxZWIwNDMt-ZjA5YjMyNmIt-w3000._CB668766246_.jpg" 
+                alt=""
+            />
+        </div>
+    )
+}
+
+export default Home
