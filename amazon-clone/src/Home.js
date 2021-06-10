27292db68d81ee30c0a254b929b14d312b2c8065
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-06-10 05:37:18 -0500

    finished banner

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div className="home">
             <img 
-                className="home_image"
+                className="home__image"
                 src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/NTYxZWIwNDMt/NTYxZWIwNDMt-ZjA5YjMyNmIt-w3000._CB668766246_.jpg" 
                 alt=""
             />
