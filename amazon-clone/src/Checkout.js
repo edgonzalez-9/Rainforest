6bd147e3f89177e83aa6144a1a3ec84871548e90
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2021-06-13 22:02:57 -0500

    worked on checkout page

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -9,7 +9,17 @@ function Checkout() {
         <div className="checkout">
             <img className="checkout__ad" 
                 src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" 
-                alt=""/>
+                alt=""
+            />
+            {basket?.length === 0 ? (
+                <div>
+                    <h2>Your Shopping Basket is empty</h2>
+                </div>
+            ) : (
+                <div>
+                    <h2>Your Shopping Basket</h2>
+                </div>
+            )}
         </div>
     )
 }
